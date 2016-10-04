92b98364831f4c330f26cbc1f6f4437387b533b4
Cary Meskell <carymeskell@gmail.com>
2016-10-04 00:20:28 -0700

    Create magicIndex.js

---
+++ magicIndex.js
@@ -0,0 +1,5 @@
+A magic index in an array A[0…n-1] is defined to be an index such that A[i] = i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A. 
+Input: Array
+Output: integer index or null
+Time complexity: O(log n)
+Space complexity: O(c)
